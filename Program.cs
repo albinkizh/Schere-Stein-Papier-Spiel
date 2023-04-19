1060054bf0d744023083381858bf62a06ab9ecc5
Crypet <chsu@test.at>
2023-04-19 08:36:23 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿var bans = new List<string>{ "Schere", "Stein", "Papier"};
+
+int index = Random.Shared.Next(bans.Count);
+
+
+
+Console.WriteLine("Willkommen zum Schere-Stein-Papier Spiel \nWähle eines der Drei aus");
+string ans = Console.ReadLine();
+
+if (ans == bans[index])
+{
+    Console.WriteLine("Unentschieden!");
+}
+else if (ans != bans[index])
+{
+    Console.WriteLine("Spieler 2 gewinnt!");
+}
+else
+{
+    Console.WriteLine("Spieler 1 gewinnt");
+}
+
+
+Console.WriteLine("Die Auswahl vom Bot war: " + bans[index]);
+
